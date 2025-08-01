e4945af2ba94a853391a542bdb95e01d91d8b9d5
lilomit <omitnatiw@gmail.com>
2025-08-01 15:01:38 +0330

    full project push - sync everything

---
+++ config.py
@@ -0,0 +1,17 @@
+# config.py
+
+# General config
+SYMBOL = "BTC-USD"
+INTERVAL = "5m"
+PERIOD = "20d"
+
+# Supertrend default parameters
+SUPERTREND_PERIOD = 10
+SUPERTREND_MULTIPLIER = 3
+
+# Advanced strategy params (if different, customize here)
+ADV_SUPERTREND_PERIOD = 7
+ADV_SUPERTREND_MULTIPLIER = 2
+
+# Initial capital for backtest
+INITIAL_CAPITAL = 10000
